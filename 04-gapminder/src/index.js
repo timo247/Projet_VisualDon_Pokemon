d494184b372622ffc8b16c03bf48eb6d77994233
Timothée Dione <timothee.dione@gmail.com>
2022-03-23 09:44:15 +0100

    test3 avec webpack et d3

---
+++ 04-gapminder/src/index.js
@@ -0,0 +1,8 @@
+import * as d3 from 'd3'
+
+// Pour importer les données
+// import file from '../data/data.csv'
+
+
+
+
---
+++ 04-gapminder/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    name: 'browser',
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            {test: /\.csv$/, loader: 'csv-loader', options: {
+                    dynamicTyping: true,
+                    header: true,
+                    skipEmptyLines: true
+                }
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
