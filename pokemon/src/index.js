cc491e02606261d2547e78510cfbeea16108c487
Karen Bonfils <karen.bonfils@heig-vd.ch>
2022-05-06 11:39:06 +0200

    Bravo?

--- pokemon/src/index.js
+++ pokemon/src/index.js
@@ -487,6 +487,8 @@ function drawForcesFaiblessesData(fetchedData, elementToDisplay) {
   //Effacement des anciens forces et faiblesses
   d3.select(".forces-faiblesses-drawn").remove();
   let forcesFaiblessesSvg = d3.select(".forces-faiblesses-svg");
+  d3.select(".faiblessesHeader").remove();
+
   forcesFaiblessesSvg
     .attr("width", forcesFaiblessesWidth + margin.left + margin.right)
     .attr("height", forcesFaiblessesHeight + margin.top + margin.bottom)
